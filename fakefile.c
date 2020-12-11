3dfd8233e28fa73404cf189edd3c987a5a6f5205
KinacOelio <ntw6232@wmich.edu>
2020-12-11 02:27:12 -0500

    a

--- fakefile.c
+++ fakefile.c
@@ -1,26 +1,69 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
+//store all recipies in recipe struct
+//check to see if passed target in recipies
+//check to see if passed target exists in wd
+//check to see if dependencies exist in wd
+//if depenencies don't exist, check to see if recipe exists for them and do that (recursive)
+//execute recipe
 
-void parse_fakefile(FILE* fakefile);
+
+void fill_targets(FILE* fakefile);
+int parse_target(char* line);
+
+struct targets
+{
+	char*** targets;
+	int* offset;
+	int n_targets;
+};
+struct targets* targets;
+
 
 void main(int argc, char* argv[])
 {
-
+	targets->n_targets = 0;
+	targets = malloc(sizeof(targets));
 	FILE* fakefile = fopen("Fakefile", "r");
-	parse_fakefile(fakefile);
-	execlp("ls", "ls", NULL);
-	printf("wew\n");
-
+	fill_targets(fakefile);
 }
 
 
-void parse_fakefile(FILE* fakefile)
+void fill_targets(FILE* fakefile)
 {
-	char buff[4096];
-	while(fgets(buff, 4096, fakefile) != NULL)
+	char line[4096];
+	int line_number = 0;
+	while(fgets(line, 4096, fakefile) != NULL)
 	{
-		printf(">%s", buff);
+		line_number++;
+		if(strlen(line) == 0 | line[0] == '\t' | line[0] == '#')
+			continue;
+		switch(parse_target(line))
+		{
+			case 0:
+				targets->offset[targets->n_targets];
+				targets->n_targets++;
+			case 1:
+				printf("there is an error on line %i\n", line_number);
+				exit(1);
+		} 
 	}
-	
+}
+
+int parse_target(char* line)
+{
+	if(line[0] == ' ' | line[0] == ':')
+		return 1;
+
+	char* tempstring;
+	int i = 0;
+	while(line[i] != '\n')
+	{
+		if(line[i] == ':')
+			strncpy(targets->targets[targets->n_targets][0], line, i);
+		i++;
+	}
 }
